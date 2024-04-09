6c19045897987d4c7a271859966eed6766924002
Marcin Szymański <ms32035@gmail.com>
2024-04-10 00:04:57 +0100

    fix site signal in tests

--- spadeapp/users/signals.py
+++ spadeapp/users/signals.py
@@ -1,10 +1,15 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.db import IntegrityError
 
 
 def setup_site(sender, **kwargs):
-    Site.objects.update_or_create(
-        pk=settings.SITE_ID,
-        domain=settings.SITE_DOMAIN,
-        name=settings.SITE_NAME,
-    )
+    try:
+        Site.objects.update_or_create(
+            pk=settings.SITE_ID,
+            domain=settings.SITE_DOMAIN,
+            name=settings.SITE_NAME,
+        )
+    # this is only needed for tests
+    except IntegrityError:
+        pass
