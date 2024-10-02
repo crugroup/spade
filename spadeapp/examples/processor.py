b6ce695ffcf3a27c360004f74068467785ba70eb
zak-nuccio <152978738+zak-nuccio@users.noreply.github.com>
2024-10-02 14:35:59 +0100

    Use user object from spadesdk 0.3.0 (#165)

--- spadeapp/examples/processor.py
+++ spadeapp/examples/processor.py
@@ -1,9 +1,12 @@
 from spadesdk.file_processor import File, FileProcessor, FileUpload
+from spadesdk.user import User
 
 
 class ExampleFileProcessor(FileProcessor):
     @classmethod
-    def process(cls, file: File, filename: str, data, user_params: dict | None) -> FileUpload:
+    def process(
+        cls, file: File, filename: str, data, user_params: dict | None, user: User, *args, **kwargs
+    ) -> FileUpload:
         """Process a file using the file processor."""
 
         return FileUpload(file=file, result=FileUpload.Result.SUCCESS, rows=1, output={"example": "output"})
