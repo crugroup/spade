5817f4ef216533931b15ec18dc902b2d4834b0eb
Zakariah Nuccio <zak.nuccio@crugroup.com>
2025-01-23 11:31:18 +0000

    Add is_active to userserializer

--- spadeapp/users/serializers.py
+++ spadeapp/users/serializers.py
@@ -32,7 +32,14 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "first_name", "last_name", "email", "groups"]
+        fields = [
+            "id",
+            "is_active",
+            "first_name",
+            "last_name",
+            "email",
+            "groups",
+        ]
 
 
 class TokenSerializer(serializers.ModelSerializer):
